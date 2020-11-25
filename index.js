8a26e8086820e7bdadc371701f924527229fdfcf
ximnoise <mrogge96@protonmail.com>
2020-11-25 17:21:57 +0100

    import models and mongoose

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Models = require('./models.js');
 
 const app = express();
+
+const Movies = Models.Movie;
+const Users = Models.User;
 
 let topMovies = [
   {
